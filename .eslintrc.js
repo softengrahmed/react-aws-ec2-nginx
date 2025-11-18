cb39d9b2de8c3dafcabfe8428891b03a48846a9d
softengrahmed <softengrahmed@gmail.com>
2025-11-18 15:00:09 -0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'playwright'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:playwright/recommended',
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'playwright/no-wait-for-timeout': 'error',
+    'playwright/no-force-option': 'warn',
+    'playwright/no-conditional-in-test': 'warn',
+    'playwright/valid-expect': 'error',
+  },
+};
